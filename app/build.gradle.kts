5f682cc5242bb1de7446958ffb596da003dbaf18
Mingyu <2776059077@qq.com>
2026-06-22 21:51:55 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.comstorss.toolbox"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.1.0"
     }
 
     buildTypes {
